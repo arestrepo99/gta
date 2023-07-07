b22ad7e7dd931bdf491dc917a50a12f55733b894
arestrepo99 <arestrepo1999@gmail.com>
2023-07-07 18:40:17 -0500

    gta car v 2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 // import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
-import {SingleTireSystem, PointMass, SpringDamper} from './spring.js';
+import {SingleTireSystem, PointMass, SpringDamper, RigidBody, MonoCar} from './spring.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -19,26 +19,6 @@ const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
 scene.add( light );	
 
 
-// Create Tire
-const tireGeometry = new THREE.CylinderGeometry( 1, 1, 1, 32 );
-// Grey color -
-const tireMaterial = new THREE.MeshLambertMaterial( {color: 'grey'} );
-const tire = new THREE.Mesh( tireGeometry, tireMaterial );
-scene.add( tire );
-
-
-tire.rotation.x = Math.PI/2;
-camera.position.z = 7;
-camera.position.y = 3;
-
-
-// Tire Mount
-const tireMountGeometry = new THREE.CylinderGeometry( 0.5, 0.5, 0.5, 32 );
-const tireMountMaterial = new THREE.MeshLambertMaterial( {color: 'grey'} );
-const tireMount = new THREE.Mesh( tireMountGeometry, tireMountMaterial );
-scene.add( tireMount );
-tireMount.position.y = 4;
-
 
 
 // define uniform data
@@ -53,7 +33,7 @@ const uniformData = {
 	}
   };
 
-const geometry = new THREE.PlaneGeometry( 80, 80 , 200, 200);
+const geometry = new THREE.PlaneGeometry( 200, 200 , 200, 200);
 // ShaderMaterial
 const material = new THREE.ShaderMaterial( {
 	wireframe: true,
@@ -66,8 +46,8 @@ const material = new THREE.ShaderMaterial( {
 			vPos = vec3(
 				position.x,
 				position.y,
-				// position.z + 1. + sin(position.x/2. + u_time )*0.5  + cos(position.y/1.0)*0.5  );
-				position.z - 1. + abs(cos((position.x + cameraPos.x)/5.)) + cos(position.y + cameraPos.y/1.0)*0.1
+				// position.z + 1. + sin(position.x/2.  )*0.5  + cos(position.y/1.0)*0.5  );
+				position.z - 1. + abs(cos((position.x + cameraPos.x)/5.))
 			);
 
 			gl_Position = projectionMatrix * modelViewMatrix * vec4( vPos, 1.0 );
@@ -84,26 +64,10 @@ const material = new THREE.ShaderMaterial( {
 const plane = new THREE.Mesh( geometry, material ).rotateX(Math.PI/2);
 scene.add( plane );
 
+const monocar = new MonoCar();
 
-
-
-// const 
-// spring.start(0, -.1);
-
-const k = 15000;
-const b = 1400;
-const l = 4;
-
-const start_height = 4;
-
-
-const massObj = new PointMass(new THREE.Vector3(0, 4 + start_height, 0), new THREE.Vector3(0, 0, 0), 1000);
-const tireObj = new PointMass(new THREE.Vector3(0, 0 + start_height, 0), new THREE.Vector3(0, 0, 0), 100);
-const springObj = new SpringDamper(k, b, l)
-
-
-const tireSystem = new SingleTireSystem(massObj, tireObj, springObj);
-
+scene.add(monocar.tire.mesh);
+scene.add(monocar.rigidBody.mesh);
 
 
 const start = window.performance.now();
@@ -113,49 +77,46 @@ let time
 
 
 
-const substeps = 1;
+const substeps = 10;
 	
 
-const input = {
-
-}
+const input = {}
 document.addEventListener('keydown', (event) => {
 	input[event.key] = true;
 });
 document.addEventListener('keyup', (event) => {
-	input[event.key] = false;
+	delete input[event.key];
 });
 
+
+monocar.rigidBody.q.multiply(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), -Math.PI/4));
 function animate(timestamp) {
 	dt = (timestamp - last)/1000; time = timestamp/1000 - start/1000; last = timestamp;
 	if (dt > 0.1) dt = 0.1;
+	
 
 
-	const masspos = tireSystem.mass.p;
-	const tirepos = tireSystem.tire.p;
-	tire.position.x = tirepos.x;
-	tire.position.y = tirepos.y;
-	tire.position.z = tirepos.z;
-
-
-	tireMount.position.x = masspos.x;
-	tireMount.position.y = masspos.y;
-	tireMount.position.z = masspos.z;
-
-
-	plane.position.x = camera.position.x;
 
 	for (let i = 0; i < substeps; i++) {
 		time += dt/substeps;
-		tireSystem.step(dt/substeps, time, input);
+		monocar.step(dt/substeps, time, input);
 	}
+	monocar.draw();
 
-	camera.position.x = tireSystem.mass.p.x;
+
+	camera.quaternion.copy(monocar.rigidBody.mesh.quaternion);
+	const q_direction_vector = new THREE.Vector3(0, -3, -15).applyQuaternion(camera.quaternion);
+	const position = monocar.rigidBody.mesh.position.clone().sub(q_direction_vector);
+	camera.position.set(position.x, position.y, position.z);
+
+	plane.position.z = camera.position.z;
+	plane.position.x = camera.position.x;
+
 	uniformData.u_time.value = time;
 	uniformData.cameraPos.value = camera.position;
+
 	renderer.render( scene, camera );
 	requestAnimationFrame( animate );
-	console.log(camera.position.x)
 }
 
 animate(start);
--- spring.js
+++ spring.js
@@ -9,7 +9,6 @@ export class PointMass {
         this.p = p;
         this.pPrev = p.clone();
         this.v = v;
-
         this.m = m;
         this.a = new THREE.Vector3(0, 0, 0);
     }
@@ -42,9 +41,9 @@ export class SpringDamper {
         this.l = l;
     }
 
-    force(point1, point2){
-        let dp = point1.p.clone().sub(point2.p);
-        let dv = point1.v.clone().sub(point2.v);
+    force(p1, p2, v1, v2){
+        let dp = p1.clone().sub(p2);
+        let dv = v1.clone().sub(v2);
         let l = dp.length();
         let f = dp.normalize().multiplyScalar(-this.k*(l-this.l)).sub(dv.multiplyScalar(this.b));
         return f;
@@ -92,6 +91,9 @@ export class SingleTireSystem {
             this.tire.p.y = floorHeight + 1;
         }
 
+        // Order constraint
+        this.mass.p.y = Math.max(this.mass.p.y, this.tire.p.y);
+
         // Make Sure the tire is always below the mass
         this.tire.p.x = this.mass.p.x
         this.tire.p.z = this.mass.p.z
@@ -108,49 +110,338 @@ export class SingleTireSystem {
 
 
 
-// export class RigidBody{
+export class RigidBody{
 
-//     constructor(mass, inertia, p, v, q, w){
-//         this.m = mass; // Mass Scalar
-//         this.I = inertia; // Moment of intertia Vetor3
-//         this.p = p; // Position Vector3
-//         this.v = v;  // Velocity Vector3
-//         this.q = q; // Quaternion
-//         this.w = w; // Angular velocity Vector3
+    constructor(mass, inertia, p, v, q, w){
+        this.m = mass; // Mass Scalar
+        this.I = inertia; // Moment of intertia Vetor3
+        this.p = p; // Position Vector3
+        this.pPrev = p.clone(); // Previous Position Vector3
+        this.v = v;  // Velocity Vector3
+        this.q = q; // Quaternion
+        this.w = w; // Angular velocity Vector3
 
-//         this.a = new THREE.Vector3(0, 0, 0);
-//         // Acceleration Vector3
-//         this.alpha = new THREE.Vector3(0, 0, 0);
-//         // Angular acceleration Vector3
-//     }
 
-//     applyForce(f, p){
-//         // f: force
-//         // p: point of application 
-//         // Torque to be applied to center of mass
+        this.a = new THREE.Vector3(0, 0, 0);
+        // Acceleration Vector3
+        this.alpha = new THREE.Vector3(0, 0, 0);
+        // Angular acceleration Vector3
 
-//         let torque = p.clone().sub(this.p).cross(f);
-//         // Force to be applied to center of mass
-//         let force = f.clone()
 
-//         this.a.add(force.clone().divideScalar(this.m));
-//         this.alpha.add(torque.clone().divide(this.I));
+        // Rectangular prism
+        this.bodyGeometry = new THREE.BoxGeometry(2, 0.1, 4);
+        // Dark red
+        this.bodyMaterial = new THREE.MeshLambertMaterial({color: 0x8B0000});
+        this.mesh = new THREE.Mesh(this.bodyGeometry, this.bodyMaterial);
+        this.mesh.position.copy(this.p);
+        this.mesh.quaternion.copy(this.q);
+        this.mesh.scale.set(1, 1, 1);
+        this.mesh.castShadow = true;
+        this.mesh.receiveShadow = true;
+
+    }
+
+    applyForce(f, p){
+        // f: force
+        // p: point of application 
+        // Torque to be applied to center of mass
+        
+        const r = p.clone().sub(this.p);
+        let torque = r.clone().cross(f);
+        let force = f.clone()
+        this.a.add(force.clone().divideScalar(this.m));
+        this.alpha.add(torque.clone().divide(this.I));
+    }
+    draw(){
+        this.mesh.position.copy(this.p);
+        this.mesh.quaternion.copy(this.q);
+    }
+
+    step(dt, time, input){
+        if (dt == 0) return;
+        // Gravity
+        this.applyForce(new THREE.Vector3(0, -9.8, 0).multiplyScalar(this.m), this.p);
+
+
+        // Apply forces relative to the direction of the quaternion
+        const directionVector = new THREE.Vector3(0, 0, -1).applyQuaternion(this.q);
+
+        if (input['ArrowUp']){
+            this.applyForce(directionVector.clone().multiplyScalar(this.m*5), this.p.clone().add(new THREE.Vector3(0, 0, 0)));
+        }
+         if (input['ArrowDown']){
+            this.applyForce(directionVector.clone().multiplyScalar(-this.m*5), this.p.clone().add(new THREE.Vector3(0, 0, 0)));
+        }
+
+        if (input['ArrowLeft']){
+            // this.applyForce(new THREE.Vector3(100, 0, 0), this.p.clone().add(new THREE.Vector3(0, 0, 0.1)));
+            this.w = new THREE.Vector3(0, 1, 0);
+        } 
+        else if (input['ArrowRight']){
+            this.w = new THREE.Vector3(0,-1, 0);
+            // this.applyForce(new THREE.Vector3(-100, 0, 0), this.p.clone().add(new THREE.Vector3(0, 0, 0.1)));
+        } else {
+            this.w = new THREE.Vector3(0, 0, 0);
+        }
+
+
+
+
+        this.w.add(this.alpha.clone().multiplyScalar(dt));
+        this.q.multiply(new THREE.Quaternion().setFromAxisAngle(this.w.clone().normalize(), this.w.length()*dt));
+        this.q.normalize();
+        this.alpha.set(0, 0, 0);
+
+        this.v = this.p.clone().sub(this.pPrev).divideScalar(dt);
+        this.pPrev = this.p.clone();
+        this.p.add(this.v.clone().multiplyScalar(dt)).add(this.a.clone().multiplyScalar(dt*dt));
+        this.a.set(0, 0, 0);
+
+    }
+
+}
+
+
+
+
+
+export class Tire {
+    constructor(p, v, m){
+        this.p = p;
+        this.pPrev = p.clone();
+        this.v = v;
+        this.m = m;
+        this.a = new THREE.Vector3(0, 0, 0);
 
         
-//     }
+        const tireGeometry = new THREE.CylinderGeometry( 1, 1, 1, 32 );
+        const tireMaterial = new THREE.MeshLambertMaterial( {color: 'grey'} );
+        this.mesh = new THREE.Mesh( tireGeometry, tireMaterial );
+        this.mesh.rotation.z = Math.PI/2;
+        this.mesh.position.copy(this.p);
 
-//     step(dt){
-//         // Gravity
-//         this.applyForce(new THREE.Vector3(0, -9.8, 0).multiplyScalar(this.m), this.p);
-//         this.v.add(this.a.clone().multiplyScalar(dt));
-//         this.p.add(this.v.clone().multiplyScalar(dt));
 
-//         this.w.add(this.alpha.clone().multiplyScalar(dt));
-//         this.q.add(new THREE.Quaternion().setFromAxisAngle(this.w.clone().normalize(), this.w.length()*dt));
-//         this.q.normalize();
+    }
+    
+    applyForce(f) {
+        this.a.add(f.clone().divideScalar(this.m));
+    }
 
-//         this.a.set(0, 0, 0);
-//         this.alpha.set(0, 0, 0);
-//     }
+    draw(){
+        this.mesh.position.copy(this.p);
+        // this.mesh.quaternion.copy(this.q);
+        this.mesh.rotation.z = Math.PI/2;
+    }
 
-// }
+    // Verlet integration
+    step(dt) {
+        // Gravity
+        if (dt == 0) return;
+        this.applyForce(new THREE.Vector3(0, -9.8, 0).multiplyScalar(this.m));
+        this.v = this.p.clone().sub(this.pPrev).divideScalar(dt);
+        this.pPrev = this.p.clone();
+        this.p.add(this.v.clone().multiplyScalar(dt)).add(this.a.clone().multiplyScalar(dt*dt));
+        this.a.set(0, 0, 0);
+    }
+
+}
+
+
+
+
+export class MonoCar{
+    
+    constructor(){
+
+        this.rigidBody = new RigidBody(
+            1000, 
+            new THREE.Vector3(100, 100, 100),
+            new THREE.Vector3(0, 5, 0),
+            new THREE.Vector3(0, 0, 0),
+            new THREE.Quaternion(0, 0, 0, 1),
+            new THREE.Vector3(0, 0, 0)
+        );
+
+        
+
+        // Create Tire
+        this.tire = new Tire(
+            new THREE.Vector3(0, 3, 0),
+            new THREE.Vector3(0, 0, 0),
+            100,
+        )
+        
+        const k = 35000;
+        const b = 3000;
+        const l = 2;
+
+        this.spring = new SpringDamper(k,b,l)
+        // Connect the tire to the tire mount
+    }
+
+    draw(){
+        this.rigidBody.draw();
+
+        this.tire.draw();
+    }
+
+    step(dt, time, input){
+
+
+        let f = this.spring.force(this.rigidBody.p, this.tire.p, this.rigidBody.v, this.tire.v);
+
+        this.rigidBody.applyForce(f, this.tire.p);
+        this.rigidBody.step(dt, time, input);
+        this.tire.applyForce(f.clone().multiplyScalar(-1));
+        this.tire.step(dt, time, input);
+
+        
+        //  Tire and tiremount constraints
+        // First move the rigid body with the force from the spring
+        const position = this.rigidBody.p
+        const quaternion = this.rigidBody.q
+        
+        // const tireMountPOsition = position.clone().add(this.relativeTireMountPosition.applyQuaternion(quaternion))
+        // this.tireMount.p = tireMountPOsition;
+    
+        this.tire.p.x = this.rigidBody.p.x;
+        this.tire.p.z = this.rigidBody.p.z;
+
+        // this.tire.p = position.clone().add(this.relativeTireMountPosition).add(this.relativeTirePosition.clone().multiplyScalar(l)).applyQuaternion(quaternion)
+        // Constraint the tire to be in the projection the vector from tireMount to tireIdealPosition
+        // const tirePosition = tireMountPOsition.clone().add(tireIdealPosition.clone().sub(tireMountPOsition).projectOnVector(tireMountPOsition.clone().sub(this.rigidBody.p)))
+
+
+        // Floor constraint
+        const tirePosition = this.tire.p;
+        const floorHeight =   1. - Math.abs(Math.cos(tirePosition.x/5.)) + Math.cos(tirePosition.y/1.0)*0.1
+        if (this.tire.p.y -1 < floorHeight){
+            this.tire.p.y = floorHeight + 1;
+        }
+
+
+        // this.tire.step(dt, time, input);
+        
+        // Order constraint
+        // this.mass.p.y = Math.max(this.mass.p.y, this.tire.p.y);
+
+        // Make Sure the tire is always below the mass
+        // this.tire.p.x = this.mass.p.x
+        // this.tire.p.z = this.mass.p.z
+
+        // if (input.l){
+        //     this.mass.applyForce(new THREE.Vector3(11*this.mass.m, 0, 0 ));
+        // }
+        // if (input.r){
+        //     this.mass.applyForce(new THREE.Vector3(-11*this.mass.m, 0, 0 ));
+        // }
+
+    }
+
+}
+    
+
+
+export class Bike{
+    
+    constructor(){
+
+        this.rigidBody = new RigidBody(
+            1000, 
+            new THREE.Vector3(100, 100, 100),
+            new THREE.Vector3(0, 5, 0),
+            new THREE.Vector3(0, 0, 0),
+            new THREE.Quaternion(0, 0, 0, 1),
+            new THREE.Vector3(0, 0, 0)
+        );
+
+        
+
+        // Create Tire
+        this.tire1 = new Tire(
+            new THREE.Vector3(0, 3, 0),
+            new THREE.Vector3(0, 0, 0),
+            100,
+        )
+        
+        // Create Tire
+        this.tire2 = new Tire(
+            new THREE.Vector3(0, 3, 0),
+            new THREE.Vector3(0, 0, 0),
+            100,
+        )
+
+        const k = 35000;
+        const b = 3000;
+        const l = 2;
+
+        this.spring1 = new SpringDamper(k,b,l)
+        this.spring2 = new SpringDamper(k,b,l)
+        // Connect the tire to the tire mount
+    }
+
+    draw(){
+        this.rigidBody.draw();
+
+        this.tire1.draw();
+        this.tire2.draw();
+    }
+
+    step(dt, time, input){
+
+
+        let f = this.spring.force(this.rigidBody.p, this.tire.p, this.rigidBody.v, this.tire.v);
+
+        this.rigidBody.applyForce(f, this.tire.p);
+        this.rigidBody.step(dt, time, input);
+        this.tire.applyForce(f.clone().multiplyScalar(-1));
+        this.tire.step(dt, time, input);
+
+        
+        //  Tire and tiremount constraints
+        // First move the rigid body with the force from the spring
+        const position = this.rigidBody.p
+        const quaternion = this.rigidBody.q
+
+        const tiremountpos = new THREE.Vector3(0, 0, 1).applyQuaternion(quaternion).add(position)
+
+        
+        // const tireMountPOsition = position.clone().add(this.relativeTireMountPosition.applyQuaternion(quaternion))
+        // this.tireMount.p = tireMountPOsition;
+    
+        this.tire.p.x = this.rigidBody.p.x + 1;
+        this.tire.p.z = this.rigidBody.p.z;
+
+        // this.tire.p = position.clone().add(this.relativeTireMountPosition).add(this.relativeTirePosition.clone().multiplyScalar(l)).applyQuaternion(quaternion)
+        // Constraint the tire to be in the projection the vector from tireMount to tireIdealPosition
+        // const tirePosition = tireMountPOsition.clone().add(tireIdealPosition.clone().sub(tireMountPOsition).projectOnVector(tireMountPOsition.clone().sub(this.rigidBody.p)))
+
+
+        // Floor constraint
+        const tirePosition = this.tire.p;
+        const floorHeight =   1. - Math.abs(Math.cos(tirePosition.x/5.)) + Math.cos(tirePosition.y/1.0)*0.1
+        if (this.tire.p.y -1 < floorHeight){
+            this.tire.p.y = floorHeight + 1;
+        }
+
+
+        // this.tire.step(dt, time, input);
+        
+        // Order constraint
+        // this.mass.p.y = Math.max(this.mass.p.y, this.tire.p.y);
+
+        // Make Sure the tire is always below the mass
+        // this.tire.p.x = this.mass.p.x
+        // this.tire.p.z = this.mass.p.z
+
+        // if (input.l){
+        //     this.mass.applyForce(new THREE.Vector3(11*this.mass.m, 0, 0 ));
+        // }
+        // if (input.r){
+        //     this.mass.applyForce(new THREE.Vector3(-11*this.mass.m, 0, 0 ));
+        // }
+
+    }
+
+}
+    
